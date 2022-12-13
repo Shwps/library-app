ce4f5e6db9490839d96fb87b391aa7bd80a73251
William Ekelund <bwekelund@gmail.com>
2022-12-13 21:18:21 +0100

    Added button in new book form for mobile users

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const modal = document.getElementById("new-book-modal");
 const exitBtn = document.getElementById("exit-btn");
 const libraryContainer = document.querySelector(".library-container");
 const addBookBtn = document.querySelector("#add-book-btn");
+const newBookForm = document.querySelector("#new-book-form");
 
 newBookBtn.addEventListener("click", displayModal);
 exitBtn.addEventListener("click", displayModal);
@@ -97,25 +98,25 @@ let card = (book) => {
   }
 
   card.onmousemove = (e) => handleOnMouseMove(e);
-  
-  infoContainer.append(title,author,year,read)
-  
-  card.append(infoContainer,edit, bookTags);
+
+  infoContainer.append(title, author, year, read);
+
+  card.append(infoContainer, edit, bookTags);
   card.classList.add("card");
   libraryContainer.appendChild(card);
   book.card = card;
 };
 
-const handleOnMouseMove = e => {
-  const { currentTarget: target} = e;
+const handleOnMouseMove = (e) => {
+  const { currentTarget: target } = e;
 
   const rect = target.getBoundingClientRect(),
-  x = e.clientX - rect.left,
-  y = e.clientY - rect.top;
+    x = e.clientX - rect.left,
+    y = e.clientY - rect.top;
 
-target.style.setProperty("--mouse-x", `${x}px`);
-target.style.setProperty("--mouse-y", `${y}px`);
-}
+  target.style.setProperty("--mouse-x", `${x}px`);
+  target.style.setProperty("--mouse-y", `${y}px`);
+};
 
 function removeBook(book) {
   myLibrary.splice(myLibrary.indexOf(book), 1);
@@ -159,41 +160,52 @@ function displayModal() {
     modal.style.display = "block";
   } else {
     modal.style.display = "none";
+    newBookForm.reset();
+    clearChildren(tagsContainer);
+    newBookTags.splice(0,newBookTags.length);
   }
 }
 
 // tags system for adding new book
 const tagInput = document.querySelector("#tag-input");
 const tagsContainer = document.querySelector(".tags");
+const addTagBtn = document.querySelector("#add-tag");
 
 tagInput.addEventListener("keydown", (e) => {
   if (e.keyCode == 13) {
-    e.preventDefault();
-    let tagText = tagInput.value;
-    if (tagText == "") {
-      return;
-    }
+    createTag(e);
+  }
+});
 
-    if (newBookTags.length >= 6) {
-      return;
-    }
-    let tag = document.createElement("div");
+addTagBtn.addEventListener("click", (e) => {
+  createTag(e);
+});
+
+function createTag(e) {
+  e.preventDefault();
+  let tagText = tagInput.value;
+  let tag = document.createElement("div");
+  if (tagText == "") {
+    return;
+  }
+  if (newBookTags.length >= 6) {
+    return;
+  }
+  if (!newBookTags.includes(tagText)) {
+    newBookTags.push(tagText);
     tag.classList.add("tag", "tag-wip");
     tag.appendChild(document.createTextNode(tagText));
     tagsContainer.appendChild(tag);
     tagInput.value = "";
-    if (!newBookTags.includes(tagText)) {
-      newBookTags.push(tagText);
-    }
-    if (!searchTags.includes(tagText)) {
-      searchTags.push(tagText);
-    }
-    tag.addEventListener("click", () => {
-      tag.remove();
-      newBookTags.splice(newBookTags.indexOf(tag.value));
-    });
   }
-});
+  if (!searchTags.includes(tagText)) {
+    searchTags.push(tagText);
+  }
+  tag.addEventListener("click", () => {
+    tag.remove();
+    newBookTags.splice(newBookTags.indexOf(tag.value));
+  });
+}
 
 const searchBar = document.querySelector("#search-bar");
 const searchTagsContainer = document.querySelector(".search-tags");
@@ -284,32 +296,6 @@ window.addEventListener("load", () => {
     localStorage.setItem("library", JSON.stringify(library));
   }
   myLibrary = JSON.parse(localStorage.getItem("library"));
-
-  // for (let i = 0; i < 1; i++) {
-  //   let bookOne = book("Lord of the Rings", "J.R.R. Tolkien", 1954, false, [
-  //     "Adventure",
-  //     "Fantasy",
-  //     "Epic",
-  //   ]);
-  //   let two = book("Pride and Prejudice", "Jane Austen", 1813, false, [
-  //     "Philosophy",
-  //     "History",
-  //     "Wit",
-  //     "Love",
-  //   ]);
-  //   let three = book("To Kill a Mockingbird", "Harper Lee", 1960, false, [
-  //     "Southern Gothic",
-  //   ]);
-  //   let four = book("The Great Gatsby", "Scott Fitzgerald", 1925, true, [
-  //     "Love",
-  //     "Betryal",
-  //     "1920's",
-  //     "Tragedy",
-  //     "Jazz Age",
-  //   ]);
-  //   myLibrary.push(bookOne, two, three, four);
-  //   localStorage.setItem("library", JSON.stringify(myLibrary));
-  // }
   displayBooksInArray();
   displaySearchTags();
 });
