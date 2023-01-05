eaf5acf458b119663c9f5e9e5ce0b88ec964a57c
William Ekelund <bwekelund@gmail.com>
2023-01-05 17:08:26 +0100

    Updated structure of javascript code, by the use of factory functions and the module pattern. Also fixed so that text search and tag filtering interact correctly

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-let myLibrary = [];
-const newBookTags = [];
-const searchTags = [];
-const selectedSearchTags = [];
+const libraryContainer = document.querySelector(".library-container");
 
 let book = (title, author, year, read, tags) => {
   let toggleRead = (readStatus) => {
@@ -15,32 +12,81 @@ let book = (title, author, year, read, tags) => {
 
 //new book button and form funcitonality
 
-const newBookBtn = document.getElementById("new-book-btn");
-const modal = document.getElementById("new-book-modal");
-const exitBtn = document.getElementById("exit-btn");
-const libraryContainer = document.querySelector(".library-container");
-const addBookBtn = document.querySelector("#add-book-btn");
-const newBookForm = document.querySelector("#new-book-form");
+const library = (() => {
+  let books = [];
+  let visibleBooks = [];
 
-newBookBtn.addEventListener("click", displayModal);
-exitBtn.addEventListener("click", displayModal);
-addBookBtn.addEventListener("click", () => {
-  addBook();
-  displayModal();
-});
+  const add = () => {
+    const book = retrivingBookInfo();
+    library.books.push(book);
+    localStorage.setItem("library", JSON.stringify(library.books));
+    card(book);
+    tags.newTags.tagsArray.splice(0, tags.newTags.tagsArray.length);
+    clearChildren(tags.newTags.container);
+    tags.search.display();
+    newBookUI.displayModal();
+  };
 
-function setReadProperty(e) {
-  let card = e.target.parentElement.parentElement.parentElement;
-  let boxValue = e.target.checked;
-  let book = myLibrary[card.dataset.bookIndex];
-  book.read = boxValue;
-  localStorage.setItem("library", JSON.stringify(myLibrary));
-}
+  const remove = (book) => {
+    library.books.splice(library.books.indexOf(book), 1);
+    clearChildren(libraryContainer);
+    localStorage.setItem("library", JSON.stringify(library.books));
+    displayBooksInArray();
+  };
+
+  const displayBooksInArray = () => {
+    for (const book of library.books) {
+      card(book);
+      for (const tag of book.tags) {
+        if (!tags.search.tags.includes(tag)) {
+          tags.search.tags.push(tag);
+        }
+      }
+    }
+  };
+
+  return { add, books, displayBooksInArray, remove, visibleBooks };
+})();
+
+const newBookUI = (() => {
+  const addBookBtn = document.querySelector("#add-book-btn");
+  const exitBtn = document.getElementById("exit-btn");
+  const form = document.querySelector("#new-book-form");
+  const modal = document.getElementById("new-book-modal");
+  const newBookBtn = document.getElementById("new-book-btn");
+
+  const displayModal = () => {
+    if (modal.style.display === "none") {
+      modal.style.display = "block";
+    } else {
+      modal.style.display = "none";
+      form.reset();
+      clearChildren(tags.newTags.container);
+      tags.newTags.tagsArray.splice(0, tags.newTags.tagsArray.length);
+    }
+  };
+
+  newBookBtn.addEventListener("click", displayModal);
+  exitBtn.addEventListener("click", displayModal);
+  addBookBtn.addEventListener("click", () => {
+    library.add();
+    displayModal();
+  });
+
+  return {
+    newBookBtn,
+    modal,
+    exitBtn,
+    addBookBtn,
+    form,
+    displayModal,
+  };
+})();
 
 //Displaying a new book
 let card = (book) => {
   let card = document.createElement("div");
-  card.dataset.bookIndex = myLibrary.indexOf(book);
+  card.dataset.bookIndex = library.books.indexOf(book);
   const infoContainer = document.createElement("div");
   infoContainer.classList.add("info-container");
 
@@ -48,7 +94,7 @@ let card = (book) => {
   edit.classList.add("edit-btn");
   edit.src = "svg/close.svg";
   edit.addEventListener("click", (e) => {
-    removeBook(book);
+    library.remove(book);
   });
   const title = document.createElement("div");
   title.classList.add("title");
@@ -80,6 +126,14 @@ let card = (book) => {
     haveReadCheckbox.checked = true;
   }
 
+  function setReadProperty(e) {
+    let card = e.target.parentElement.parentElement.parentElement;
+    let boxValue = e.target.checked;
+    let book = library.books[card.dataset.bookIndex];
+    book.read = boxValue;
+    localStorage.setItem("library", JSON.stringify(library.books));
+  }
+
   haveReadCheckbox.addEventListener("change", setReadProperty);
 
   readStatusText.appendChild(textNode);
@@ -105,6 +159,7 @@ let card = (book) => {
   card.classList.add("card");
   libraryContainer.appendChild(card);
   book.card = card;
+  library.visibleBooks.push(book);
 };
 
 const handleOnMouseMove = (e) => {
@@ -118,24 +173,6 @@ const handleOnMouseMove = (e) => {
   target.style.setProperty("--mouse-y", `${y}px`);
 };
 
-function removeBook(book) {
-  myLibrary.splice(myLibrary.indexOf(book), 1);
-  clearChildren(libraryContainer);
-  localStorage.setItem("library", JSON.stringify(myLibrary));
-  displayBooksInArray();
-}
-
-function addBook() {
-  const book = retrivingBookInfo();
-  myLibrary.push(book);
-  localStorage.setItem("library", JSON.stringify(myLibrary));
-  card(book);
-  newBookTags.splice(0, newBookTags.length);
-  clearChildren(tagsContainer);
-  displaySearchTags();
-  displayModal();
-}
-
 function clearChildren(parent) {
   let child = parent.lastElementChild;
   while (child) {
@@ -144,150 +181,165 @@ function clearChildren(parent) {
   }
 }
 
-function displayBooksInArray() {
-  for (const book of myLibrary) {
-    card(book);
-    for (const tag of book.tags) {
-      if (!searchTags.includes(tag)) {
-        searchTags.push(tag);
+// tags system for filtering books
+const tags = (() => {
+  //tags system for when adding new book and new tags
+  const newTags = (() => {
+    const tagsArray = [];
+
+    const tagInput = document.querySelector("#tag-input");
+    const container = document.querySelector(".tags");
+    const addTagBtn = document.querySelector("#add-tag");
+
+    function createTag(e) {
+      e.preventDefault();
+      let tagText = tagInput.value;
+      let tag = document.createElement("div");
+      if (tagText == "") {
+        return;
+      }
+      if (tagsArray.length >= 6) {
+        return;
+      }
+      if (!tagsArray.includes(tagText)) {
+        tagsArray.push(tagText);
+        tag.classList.add("tag", "tag-wip");
+        tag.appendChild(document.createTextNode(tagText));
+        container.appendChild(tag);
+        tagInput.value = "";
+      }
+      if (!tags.search.tags.includes(tagText)) {
+        tags.search.tags.push(tagText);
+      }
+      tag.addEventListener("click", () => {
+        tag.remove();
+        tagsArray.splice(tagsArray.indexOf(tag.value));
+      });
+    }
+
+    //event listeners
+    tagInput.addEventListener("keydown", (e) => {
+      if (e.keyCode == 13) {
+        createTag(e);
+      }
+    });
+
+    addTagBtn.addEventListener("click", (e) => {
+      createTag(e);
+    });
+
+    return { container, tagsArray };
+  })();
+
+  //Tag system for filtering books while searching
+  const search = (() => {
+    const tags = [];
+    const selectedTags = [];
+    const container = document.querySelector(".search-tags");
+    const toggleTags = document.querySelector("#toggle-tags");
+
+    let isTagsShowing = false;
+
+    toggleTags.addEventListener("click", () => {
+      const searchTags = container.childNodes;
+      if (isTagsShowing) {
+        for (let tag of searchTags) {
+          if (Array.from(searchTags).indexOf(tag) >= 13) {
+            tag.style.display = "none";
+          }
+        }
+        toggleTags.style.transform = "rotate(0deg)";
+        isTagsShowing = false;
+      } else {
+        for (let tag of searchTags) {
+          tag.style.display === "none"
+            ? (tag.style.display = "block")
+            : (tag.style.display = "block");
+        }
+        toggleTags.style.transform = "rotate(180deg)";
+        isTagsShowing = true;
+      }
+    });
+
+    function display() {
+      clearChildren(container);
+      tags.sort();
+      for (const tag of tags) {
+        let tagElement = document.createElement("div");
+        tagElement.classList.add("tag", "search-tag");
+        tagElement.appendChild(document.createTextNode(tag));
+        if (container.childNodes.length > 14) {
+          tagElement.style.display = "none";
+        }
+        container.append(tagElement);
       }
     }
-  }
-}
 
-function displayModal() {
-  if (modal.style.display === "none") {
-    modal.style.display = "block";
-  } else {
-    modal.style.display = "none";
-    newBookForm.reset();
-    clearChildren(tagsContainer);
-    newBookTags.splice(0,newBookTags.length);
-  }
-}
-
-// tags system for adding new book
-const tagInput = document.querySelector("#tag-input");
-const tagsContainer = document.querySelector(".tags");
-const addTagBtn = document.querySelector("#add-tag");
-
-tagInput.addEventListener("keydown", (e) => {
-  if (e.keyCode == 13) {
-    createTag(e);
-  }
-});
-
-addTagBtn.addEventListener("click", (e) => {
-  createTag(e);
-});
-
-function createTag(e) {
-  e.preventDefault();
-  let tagText = tagInput.value;
-  let tag = document.createElement("div");
-  if (tagText == "") {
-    return;
-  }
-  if (newBookTags.length >= 6) {
-    return;
-  }
-  if (!newBookTags.includes(tagText)) {
-    newBookTags.push(tagText);
-    tag.classList.add("tag", "tag-wip");
-    tag.appendChild(document.createTextNode(tagText));
-    tagsContainer.appendChild(tag);
-    tagInput.value = "";
-  }
-  if (!searchTags.includes(tagText)) {
-    searchTags.push(tagText);
-  }
-  tag.addEventListener("click", () => {
-    tag.remove();
-    newBookTags.splice(newBookTags.indexOf(tag.value));
-  });
-}
-
-const searchBar = document.querySelector("#search-bar");
-const searchTagsContainer = document.querySelector(".search-tags");
-const toggleTags = document.querySelector("#toggle-tags");
-let isTagsShowing = false;
-const SearchTagsArray = [];
-
-searchBar.addEventListener("input", (e) => {
-  const value = searchBar.value.toLowerCase();
-  myLibrary.forEach((book) => {
-    const isVisible =
-      book.title.toLowerCase().includes(value) ||
-      book.author.toLowerCase().includes(value);
-    book.card.classList.toggle("hide", !isVisible);
-  });
-});
-
-toggleTags.addEventListener("click", (e) => {
-  const searchTags = searchTagsContainer.childNodes;
-  if (isTagsShowing) {
-    for (let tag of searchTags) {
-      if (Array.from(searchTags).indexOf(tag) >= 13) {
-        tag.style.display = "none";
+    container.addEventListener("click", (e) => {
+      if (!e.target.classList.contains("tag")) {
+        return;
       }
-    }
-    toggleTags.style.transform = "rotate(0deg)";
-    isTagsShowing = false;
-  } else {
-    for (let tag of searchTags) {
-      tag.style.display === "none"
-        ? (tag.style.display = "block")
-        : (tag.style.display = "block");
-    }
-    toggleTags.style.transform = "rotate(180deg)";
-    isTagsShowing = true;
-  }
-});
+      filtering(e);
+    });
 
-function displaySearchTags() {
-  clearChildren(searchTagsContainer);
-  searchTags.sort();
-  for (const tag of searchTags) {
-    let tagElement = document.createElement("div");
-    tagElement.classList.add("tag", "search-tag");
-    tagElement.appendChild(document.createTextNode(tag));
-    if (searchTagsContainer.childNodes.length > 14) {
-      tagElement.style.display = "none";
-    }
-    searchTagsContainer.append(tagElement);
-  }
-}
+    let filtering = (e) => {
+      const tag = e.target;
+      const tagText = tag.textContent;
 
-searchTagsContainer.addEventListener("click", (e) => {
-  if (!e.target.classList.contains("tag")) {
-    return;
-  }
+      tag.classList.toggle("selected");
+      selectedTags.includes(tagText)
+        ? selectedTags.splice(selectedTags.indexOf(tagText), 1)
+        : selectedTags.push(tagText);
+      library.books.forEach((book) => {
+        let counter = 0;
+        for (let selectedTag of selectedTags) {
+          if (book.tags.includes(selectedTag)) {
+            counter++;
+          }
+        }
+        let isVisible = counter == selectedTags.length;
+        book.card.classList.toggle("hide", !isVisible);
 
-  const tag = e.target;
-  const tagText = tag.textContent;
-  tag.classList.toggle("selected");
-  selectedSearchTags.includes(tagText)
-    ? selectedSearchTags.splice(selectedSearchTags.indexOf(tagText), 1)
-    : selectedSearchTags.push(tagText);
-  myLibrary.forEach((book) => {
-    let counter = 0;
-    for (let selectedTag of selectedSearchTags) {
-      if (book.tags.includes(selectedTag)) {
-        counter++;
-      }
-    }
-    let isVisible = counter == selectedSearchTags.length;
-    book.card.classList.toggle("hide", !isVisible);
+        if (isVisible) {
+          if (!library.visibleBooks.includes(book)) {
+            library.visibleBooks.push(book);
+          }
+        } else {
+          if (library.visibleBooks.includes(book)) {
+            library.visibleBooks.splice(library.visibleBooks.indexOf(book), 1);
+          }
+        }
+      });
+    };
+
+    return { container, display, tags, selectedTags };
+  })();
+
+  return { newTags, search };
+})();
+
+const search = (() => {
+  const searchBar = document.querySelector("#search-bar");
+
+  searchBar.addEventListener("input", () => {
+    const value = searchBar.value.toLowerCase();
+    library.visibleBooks.forEach((book) => {
+      const isVisible =
+        book.title.toLowerCase().includes(value) ||
+        book.author.toLowerCase().includes(value);
+      book.card.classList.toggle("hide", !isVisible);
+    });
   });
-});
+})();
 
 function retrivingBookInfo() {
   const title = document.getElementById("new-book-title").value;
   const author = document.getElementById("new-book-author").value;
   const year = parseInt(document.getElementById("new-book-year").value);
   let read = document.getElementById("new-book-read").checked;
+  let bookTags = structuredClone(tags.newTags.tagsArray);
 
-  return book(title, author, year, read, newBookTags);
+  return book(title, author, year, read, bookTags);
 }
 
 window.addEventListener("load", () => {
@@ -295,9 +347,9 @@ window.addEventListener("load", () => {
     const library = [];
     localStorage.setItem("library", JSON.stringify(library));
   }
-  myLibrary = JSON.parse(localStorage.getItem("library"));
-  displayBooksInArray();
-  displaySearchTags();
+  library.books = JSON.parse(localStorage.getItem("library"));
+  library.displayBooksInArray();
+  tags.search.display();
 });
 
 const demoBtn = document.getElementById("demo-btn");
@@ -309,9 +361,9 @@ demoBtn.addEventListener("click", () => {
     })
     .then(function (data) {
       let demoLibrary = JSON.parse(data);
-      myLibrary.push.apply(myLibrary, demoLibrary);
-      localStorage.setItem("library", JSON.stringify(myLibrary));
-      displayBooksInArray();
-      displaySearchTags();
+      library.books.push.apply(library.books, demoLibrary);
+      localStorage.setItem("library", JSON.stringify(library.books));
+      library.displayBooksInArray();
+      tags.search.display();
     });
 });
